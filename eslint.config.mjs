92b3e08fcaea778dfc8ddbbb7d3b263e4c611afd
Miguel Jesus <miguelkjesus05@gmail.com>
2026-06-27 22:30:05 +0100

    init: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,69 @@
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import ts from 'typescript-eslint'
+
+export default defineConfig(
+	{ ignores: ['out/**/*'] },
+
+	// js/ts lint settings
+	js.configs.recommended,
+	...ts.configs.recommendedTypeChecked,
+	...ts.configs.stylisticTypeChecked,
+
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ['eslint.config.mjs'],
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
+			'@typescript-eslint/no-namespace': 'off',
+			// Disable rules that conflict with regular tsc type checking
+			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/unbound-method': 'off',
+			'@typescript-eslint/no-unsafe-member-access': 'off',
+			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/no-unsafe-call': 'off',
+			'@typescript-eslint/no-unsafe-error': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
+		},
+	},
+
+	// Import sorting
+	importPlugin.flatConfigs.recommended,
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					alwaysTryTypes: true,
+				},
+			},
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+			'import/first': 'error',
+			'import/newline-after-import': 'error',
+			'import/no-duplicates': 'error',
+		},
+	},
+
+	// Must be last
+	prettier,
+)
