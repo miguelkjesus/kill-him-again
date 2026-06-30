538eb4e440311523e215dc857a3e2938ade50c18
Miguel Jesus <miguelkjesus05@gmail.com>
2026-06-30 21:08:21 +0100

    feat: disable unnecessary core gui

---
+++ src/shared-first/main.client.ts
@@ -0,0 +1,6 @@
+const StarterGui = game.GetService('StarterGui')
+
+StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.SelfView, false)
+StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.AvatarSwitcher, false)
