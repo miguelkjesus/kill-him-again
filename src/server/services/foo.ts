0e6539a9325170fde9d5c1a86f950d7b420e48a6
Miguel Jesus <miguelkjesus05@gmail.com>
2026-07-02 17:31:03 +0100

    feat: add OnPreRender service event and make OnStart optional

--- src/server/services/foo.ts
+++ src/server/services/foo.ts
@@ -2,12 +2,4 @@ import { Service } from 'shared/modules/service'
 import { Services } from 'shared/modules/service-registry'
 
 @Services.Declare
-export class FooService extends Service {
-	protected OnStart() {
-		print('FooService started')
-	}
-
-	protected OnStop() {
-		print('FooService stopped')
-	}
-}
+export class FooService extends Service {}
